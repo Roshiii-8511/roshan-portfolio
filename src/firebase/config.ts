83cf59bfd5e992dd3a181db1cc6ee7847ea97d6d
SONAM KASHYAP <sonamkashyap0067@gmail.com>
2026-03-10 16:04:19 +0000

    const firebaseConfig = {   apiKey: "AIzaSyDegnjBx0MPjQDxAiMu6s3j3Opm4l8I

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSy...", // This will be provided by your Firebase console
+  apiKey: "AIzaSyDegnjBx0MPjQDxAiMu6s3j3Opm4l8I4Y4",
   authDomain: "portfolio-abc19.firebaseapp.com",
   projectId: "portfolio-abc19",
-  storageBucket: "portfolio-abc19.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  storageBucket: "portfolio-abc19.firebasestorage.app",
+  messagingSenderId: "275304851852",
+  appId: "1:275304851852:web:99549879ce83897ba58ea9"
 };
